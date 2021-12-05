7fa225cf36c80ac32a80c11959d80da8f6b5520a
Victor Vermeulen <vcvermeulen@gmail.com>
2021-12-05 17:44:06 +0100

    add first input field

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,36 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <FootballData />
+    <input
+      v-model="pincode"
+      class="
+        shadow
+        appearance-none
+        border
+        rounded
+        w-full
+        py-2
+        px-3
+        text-gray-700
+        leading-tight
+        focus:outline-none focus:shadow-outline
+      "
+    />
+    <p>{{ pincode }}</p>
+    <button
+      v-on:click="verify"
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded
+      "
+    >
+      Login
+    </button>
   </div>
 </template>
 
@@ -16,6 +46,22 @@ export default {
   components: {
     HelloWorld,
     FootballData,
+  },
+  data() {
+    return {
+      digit: '',
+      //store the pincode
+      pincode: '',
+    }
+  },
+  methods: {
+    updatePincode() {
+      this.pincode = this.digit
+    },
+    verify() {
+      //if the code is correct, push to page with teams etc.
+      this.$router.push('/about')
+    },
   },
 }
 </script>
